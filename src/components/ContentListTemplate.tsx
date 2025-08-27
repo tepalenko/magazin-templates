7ca10e32efeb52198ce6a2ce1aab2815f0c2b639
Dima Tepalenko <dmytro@low6.co.uk>
2025-08-27 18:14:50 +0200

    minor fix in contest list

--- src/components/ContentListTemplate.tsx
+++ src/components/ContentListTemplate.tsx
@@ -10,6 +10,21 @@ interface ContentListTemplateProps {
     item: IssueItem
     index: number
 }
+const getBorderRadius = (index: number) => {
+    if (index === 0) return '0 0 4px 0'
+    if (index === 1) return '0 0 0 4px'
+    if (index === 2) return '0 4px 0 0'
+    if (index === 3) return '4px 0 0 0'
+    if (index === 4) return '0 4px 0 0'
+
+    return '0'
+}
+
+const getPaddingLeft = (index: number) => {
+    console.log('getPaddingLeft', index, index % 2 === 0 ? 0 : 1);
+    return index % 2 === 0 ? 0 : 1;
+
+}
 
 const ContentListTemplate: React.FC<ContentListTemplateProps> = ({ item }) => {
     return (
@@ -19,18 +34,39 @@ const ContentListTemplate: React.FC<ContentListTemplateProps> = ({ item }) => {
                     <Grid item key={idx} sx={{
                         width: 'calc(50% - 0.5px)',
                         p: 0,
+                        py: 1,
                         backgroundColor: '#fff',
                         overflow: 'hidden',
                         ml: idx % 2 === 1 ? '1px' : 0,
-                        mb: '1px'
+                        mb: '1px',
+                        borderRadius: getBorderRadius(idx),
+                        pl: getPaddingLeft(idx)
                     }}>
                         <Grid container>
                             <Grid item xs={6}>
-                                <Box sx={{ width: '100%', height: 50, backgroundImage: `url(https://imagedelivery.net/sh0QUHFZwG_8L-du34ou1A/e495b61f-cf97-4bd8-4136-0f2f3e25b400/original)`, backgroundSize: 'cover', backgroundPosition: 'center' }} />
+                                <Box sx={{
+                                    width: '100%',
+                                    height: '100%',
+                                    backgroundImage: `url(https://imagedelivery.net/sh0QUHFZwG_8L-du34ou1A/e495b61f-cf97-4bd8-4136-0f2f3e25b400/original)`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                }} />
                             </Grid>
                             <Grid item xs={6}>
-                                <Typography variant="h6" sx={{ px: 1, fontFamily: 'Fixel Text, Arial, sans-serif', textTransform: 'uppercase' }}>{img}</Typography>
-                                <Typography variant="body2" sx={{ px: 1, fontFamily: 'Fixel Text, Arial, sans-serif' }}>Lorem ipsum dolor sit amet
+                                <Typography variant="h6" sx={{
+                                    px: 1,
+                                    fontFamily: 'Fixel Text, Arial, sans-serif',
+                                    textTransform: 'uppercase',
+                                    fontSize: '1rem',
+                                    lineHeight: '1rem',
+                                    mb: 0.5
+                                }}>{img}</Typography>
+                                <Typography variant="body2" sx={{
+                                    px: 1,
+                                    fontFamily: 'Fixel Text, Arial, sans-serif',
+                                    fontSize: '0.65rem',
+                                    lineHeight: '0.55rem',
+                                }}>Lorem ipsum dolor sit amet
                                     consectetur adipiscing elit.
                                 </Typography>
                             </Grid>
@@ -38,7 +74,7 @@ const ContentListTemplate: React.FC<ContentListTemplateProps> = ({ item }) => {
                     </Grid>
                 ))}
             </Grid>
-        </Box>
+        </Box >
     )
 }
 
